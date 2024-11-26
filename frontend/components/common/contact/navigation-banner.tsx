dc43583666de1c71f225c85f4cf9ae26d003578b
super-user <winzaw.oo@smilaxglobal.com>
2024-11-26 22:15:53 +0630

    refactor: navigation banner links to map list

--- frontend/components/common/contact/navigation-banner.tsx
+++ frontend/components/common/contact/navigation-banner.tsx
@@ -1,21 +1,21 @@
 import Link from 'next/link'
 import React from 'react'
 
+const links = [
+    { id: 1, name: 'Book a Demo', href: '#' },
+    { id: 2, name: 'Enquiry for Partnership', href: '#' },
+    { id: 3, name: 'Helpdesk Support', href: '#' },
+    { id: 4, name: 'Project Team Support', href: '#' }
+]
+
 export default function NavigationBanner() {
     return (
         <div className='w-fit mx-auto my-5 text-2xl font-bold leading-8 tracking-normal '>
-            <Link href={'#'} className='inline-block mx-10 hover:text-primary hover:underline' >
-                Book a Demo
-            </Link>
-            <Link href={'#'} className='inline-block mx-10 hover:text-primary hover:underline'>
-                Enquiry for Partnership
-            </Link>
-            <Link href={'#'} className='inline-block mx-10 hover:text-primary hover:underline'>
-                Helpdesk Support
-            </Link>
-            <Link href={'#'} className='inline-block mx-10 hover:text-primary hover:underline'>
-                Project Team Support
-            </Link>
+            {links.map(lnk => (
+                <Link key={lnk.id} href={lnk.href} className='inline-block mx-10 hover:text-primary hover:underline' >
+                    {lnk.name}
+                </Link>
+            ))}
         </div>
     )
 }
