0dd2bafdfe4d8218d4466ad308fa3518c28c8959
super-user <winzaw.oo@smilaxglobal.com>
2024-12-01 00:20:09 +0630

    feat: form layout and set up image

--- frontend/app/contact/page.tsx
+++ frontend/app/contact/page.tsx
@@ -1,13 +1,23 @@
 import ContactForm from '@/components/common/contact/form'
 import NavigationBanner from '@/components/common/contact/navigation-banner'
 import PageContainer from '@/components/common/page-container'
+import Image from 'next/image'
+import DoctorPointing from '@/assets/imgs/doctor-pointing.png'
+
 
 export default function ContactPage() {
     return (
-        <PageContainer>
+        <PageContainer className='mx-auto'>
             <NavigationBanner />
-            <div className='mx-auto'>
+            <div className='flex items-center justify-around mx-9'>
                 <ContactForm />
+                <Image
+                    src={DoctorPointing}
+                    alt='Doctor Pointing'
+                    height={300}
+                    width={300}
+                    className='w-2/5 max-h-[30em]'
+                />
             </div>
         </PageContainer>
     )
