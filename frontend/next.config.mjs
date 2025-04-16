f8289cb41ab3e0545f8a2d66767463cec96a1245
WinZawOo <winzawoo.dev@gmail.com>
2025-04-16 22:34:33 +0630

    update deployment config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  output: 'standalone',
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
@@ -10,9 +11,24 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
+        protocol: "https",
+        hostname: "tranquil-badge-60175d2313.media.strapiapp.com",
+      },
+      {
         protocol: "http",
         hostname: "localhost",
         port: "1337", // Optional: specify the port
+        pathname: "/**", // Optional: specify the path patterns
+      },
+	  {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "1337", // Optional: specify the port
+        pathname: "/**", // Optional: specify the path patterns
+      },
+      {
+        protocol: "https",
+        hostname: "tranquil-badge-60175d2313.strapiapp.com",
         pathname: "/**", // Optional: specify the path patterns
       },
       {
